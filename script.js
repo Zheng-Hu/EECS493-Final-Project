47b4d3fb55d33495a5490bf49e88f755fb98a7ce
Bennett Lawson <bclaw@umich.edu>
2021-11-08 14:06:21 -0500

    Created vue instance

---
+++ script.js
@@ -0,0 +1,20 @@
+var app_view = new Vue({
+    el: '#app',
+    data: {
+        showSignIn: true,
+        showCreateAccount: false,
+        showFeed: false,
+        showMakeAPost: false,
+        showLeaderboard: false,
+        showProfile: false,
+    },
+    methods: {
+
+    },
+    computed: {
+
+    },
+    mounted() {
+
+    }
+})
