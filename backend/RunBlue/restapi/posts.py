fa3ea13ccb0d43b62a8f54c13b9d7b81ac00dd91
zheng hu <zhenghu@umich.edu>
2021-11-12 22:07:53 -0500

    Change database and finish post api

--- backend/RunBlue/restapi/posts.py
+++ backend/RunBlue/restapi/posts.py
@@ -2,20 +2,41 @@ import flask
 import RunBlue
 
 
-@RunBlue.app.route('/')
-def show_index():
-    """Display / route."""
+@RunBlue.app.route('/api/v1/posts/', methods =["GET"])
+def posts():
+  db = RunBlue.model.get_db()
+  size = flask.request.args.get("size",default=10,type=int)
+  page = flask.request.args.get("page",default=0,type=int)
+    
+  url = "/api/v1/posts/"
+  context = {
+      "url": url
+  }
+  cur = db.execute(
+                   "SELECT posts.postid AS created, posts.owner AS owner, posts.time AS time, posts.distance AS distance, "
+                   "posts.caption AS caption, posts.created AS created " 
+                   "FROM posts ORDER BY created DESC")
+                  #  "ORDER BY created DESC, postid DESC LIMIT ? OFFSET ? * ?",\
+                  #  (size,size,page,))
+  context['data'] = list(cur.fetchall())
+  # if size*(page+1) >= cur.fetchall()[0]["num"]:
+  #   context['next'] = ''
+  # else:
+  #   context['next'] = f"/api/v1/p/?size={size}&page={page+1}"
+  return flask.jsonify(**context);
 
-    # Connect to database
-    connection = RunBlue.model.get_db()
+@RunBlue.app.route('/api/v1/posts/<int:postid>/', methods=["GET"])
+def get_post(postid):
+  url = "/api/v1/posts/" + str(postid) + "/"
+  context = {
+      "url": url
+  }
+  db = RunBlue.model.get_db()
+  cur = db.execute("SELECT posts.postid AS created, posts.owner AS owner, posts.time AS time, posts.distance AS distance, "
+                   "posts.caption AS caption, posts.created AS created " 
+                   "FROM posts WHERE posts.postid=?",(postid,))
+  context['data'] = list(cur.fetchall())
+  return flask.jsonify(**context);
 
-    # Query database
-    cur = connection.execute(
-        "SELECT username "
-        "FROM users"
-    )
-    users = cur.fetchall()
 
-    # Add database info to context
-    context = {"users": users}
-    return flask.jsonify(**context)
+
--- backend/sql/data.sql
+++ backend/sql/data.sql
@@ -7,12 +7,8 @@ values ('awdeorio', '123456'),
     ('jag', '123456');
 
 
-insert into posts(postid,filename,owner)
-values (1, '122a7d27ca1d7420a1072f695d9290fad4501a41.jpg',
-'awdeorio'),
-(2, 'ad7790405c539894d25ab8dcf0b79eed3341e109.jpg',
-'jflinn'),
-(3, '9887e06812ef434d291e4936417d125cd594b38a.jpg',
-'awdeorio'),
-(4, '2ec7cf8ae158b3b1f40065abfb33e81143707842.jpg',
-'jag');
+insert into posts(postid,filename,owner,time, distance, caption)
+values (2, '122a7d27ca1d7420a1072f695d9290fad4501a41.jpg',
+'awdeorio',8.3,1.2,'Took a run through downtown!'),
+(6, 'ad7790405c539894d25ab8dcf0b79eed3341e109.jpg',
+'jflinn',20.9,2.7,'My run from Monday')
