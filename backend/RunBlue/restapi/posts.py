aab29b400899f30f7f76d1fba543ffc8abaeb9a6
zheng hu <zhenghu@umich.edu>
2021-11-13 20:07:45 -0500

    Finish the backend restapi

--- backend/RunBlue/restapi/posts.py
+++ backend/RunBlue/restapi/posts.py
@@ -1,4 +1,5 @@
 import flask
+from flask import jsonify,request
 import RunBlue
 
 
@@ -12,18 +13,29 @@ def posts():
   context = {
       "url": url
   }
-  cur = db.execute(
-                   "SELECT posts.postid AS created, posts.owner AS owner, posts.time AS time, posts.distance AS distance, "
-                   "posts.caption AS caption, posts.created AS created " 
-                   "FROM posts ORDER BY created DESC")
-                  #  "ORDER BY created DESC, postid DESC LIMIT ? OFFSET ? * ?",\
-                  #  (size,size,page,))
-  context['data'] = list(cur.fetchall())
-  # if size*(page+1) >= cur.fetchall()[0]["num"]:
-  #   context['next'] = ''
-  # else:
-  #   context['next'] = f"/api/v1/p/?size={size}&page={page+1}"
-  return flask.jsonify(**context);
+  username = request.args.get("user")
+  if (username):
+    cur = db.execute(
+                    "SELECT posts.postid AS created, posts.owner AS owner, posts.time AS time, posts.distance AS distance, "
+                    "posts.caption AS caption, posts.created AS created " 
+                    "FROM posts WHERE posts.owner = ? ORDER BY created DESC",\
+                    (username,)
+                    )
+    context['data'] = list(cur.fetchall())
+    return flask.jsonify(**context);
+  else:
+    cur = db.execute(
+                      "SELECT posts.postid AS created, posts.owner AS owner, posts.time AS time, posts.distance AS distance, "
+                      "posts.caption AS caption, posts.created AS created " 
+                      "FROM posts ORDER BY created DESC")
+                    #  "ORDER BY created DESC, postid DESC LIMIT ? OFFSET ? * ?",\
+                    #  (size,size,page,))
+    context['data'] = list(cur.fetchall())
+    # if size*(page+1) >= cur.fetchall()[0]["num"]:
+    #   context['next'] = ''
+    # else:
+    #   context['next'] = f"/api/v1/p/?size={size}&page={page+1}"
+    return flask.jsonify(**context);
 
 @RunBlue.app.route('/api/v1/posts/<int:postid>/', methods=["GET"])
 def get_post(postid):
@@ -39,4 +51,24 @@ def get_post(postid):
   return flask.jsonify(**context);
 
 
-
+@RunBlue.app.route('/api/v1/leaderboard/', methods=["GET"])
+def leaderboard():
+  username = request.args.get("user")
+  db = RunBlue.model.get_db()
+  if (username):
+    url = "/api/v1/leaderboard/"
+    context = {"url": url}
+    cur = db.execute("SELECT workout.workoutid AS workoutid, workout.time AS time, workout.distance AS distance, workout.owner AS owner "
+                  "FROM workout WHERE workout.owner = ? ORDER BY created DESC",\
+                    (username,)
+                  )
+    context['data'] = list(cur.fetchall())
+    return flask.jsonify(**context); 
+  else:
+    url = "/api/v1/leaderboard/"
+    context = {"url": url}
+    cur = db.execute("SELECT workout.workoutid AS workoutid, workout.time AS time, workout.distance AS distance, workout.owner AS owner "
+                  "FROM workout ORDER BY created DESC")
+    context['data'] = list(cur.fetchall())
+    return flask.jsonify(**context); 
+  
--- backend/sql/data.sql
+++ backend/sql/data.sql
@@ -4,11 +4,16 @@ insert into users(username,password)
 values ('awdeorio', '123456'),
     ('jflinn', '123456'),
     ('michjc', '123456'),
-    ('jag', '123456');
+    ('jag', '123456'),
+    ('user1' , '123456'),
+    ('user2', '123456');
 
 
 insert into posts(postid,filename,owner,time, distance, caption)
 values (2, '122a7d27ca1d7420a1072f695d9290fad4501a41.jpg',
 'awdeorio',8.3,1.2,'Took a run through downtown!'),
 (6, 'ad7790405c539894d25ab8dcf0b79eed3341e109.jpg',
-'jflinn',20.9,2.7,'My run from Monday')
+'jflinn',20.9,2.7,'My run from Monday');
+
+insert into workout(workoutid,time,distance,owner)
+values (2, 8.3, 1.2, 'user1'), (1, 15, 2, 'user2')
