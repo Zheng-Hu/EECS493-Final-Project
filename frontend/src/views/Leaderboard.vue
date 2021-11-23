0859ce32306b331716ba051fe60a4b0accabd4eb
rheidem <rheidem@umich.edu>
2021-11-23 01:22:13 -0500

    Add points leaderboard

--- frontend/src/views/Leaderboard.vue
+++ frontend/src/views/Leaderboard.vue
@@ -4,7 +4,7 @@
             <Navigator page="Leaderboard"/>
         </div>
         <div class="row justify-content-center">
-            <h1 class="display-4 py-2 text-truncate text-center my-3">Leaderboard</h1>
+            <h1 class="display-4 py-2 text-truncate text-center my-3">Workout Leaderboard</h1>
             <table class="table table-striped table-bordered w-50">
                 <thead>
                     <tr>
@@ -24,6 +24,23 @@
                 </tbody>
                 </table>
         </div>
+        <div class="row justify-content-center">
+            <h1 class="display-4 py-2 text-truncate text-center my-3">Points Leaderboard</h1>
+            <table class="table table-striped table-bordered w-50">
+                <thead>
+                    <tr>
+                        <th scope="col">User</th>
+                        <th scope="col">Points</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(user, index) in users" :key="user.username" :id="index">
+                        <th>{{ user.username }}</th>
+                        <th>{{ user.points }}</th>
+                    </tr>
+                </tbody>
+                </table>
+        </div>
     </div>
 </template>
 
@@ -35,7 +52,8 @@ export default {
     name: 'Leaderboard',
     data: function () {
         return {
-            workouts: []
+            workouts: [],
+            users: []
         }
     },
     beforeCreate() {
@@ -44,7 +62,7 @@ export default {
         }
     },
     created() {
-        // Call API to get leaderboard
+        // Call API to get workouts leaderboard
         this.axios({
             method: 'get',
             url: 'http://localhost:8080/api/v1/leaderboard/'
@@ -57,6 +75,21 @@ export default {
             }
 
             console.log(this.workouts);
+        })
+        .catch(function (error) {
+            // API call failed
+            console.log(error);
+        });
+
+        // Call API to get users leaderboard
+        this.axios({
+            method: 'get',
+            url: 'http://localhost:8080/api/v1/leaderboard/points/'
+        })
+        .then(response => {
+            // Successful API call, add place to each workout
+            this.users = response.data.data;
+            console.log(this.users);
         })
         .catch(function (error) {
             // API call failed
