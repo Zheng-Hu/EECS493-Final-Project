1bb5dd808a06615f0dfa387911eba65f1bf9b11b
rheidem <rheidem@umich.edu>
2021-11-22 23:05:19 -0500

    Fix page title and move logout button to right side of nav

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,15 @@
 <template>
     <div id="app">
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand m-2">RunBlue</a>
-            <button v-if="this.$currentUser.username !== ''" v-on:click="logoutUser" class="btn btn-secondary">logout</button>
+            <div class="row m-1 w-100">
+                <div class="col-1">
+                    <a class="navbar-brand m-2">RunBlue</a>
+                </div>
+                <div class="col-10"></div>
+                <div class="col-1">
+                    <button v-if="this.$currentUser.username !== ''" v-on:click="logoutUser" class="btn btn-secondary">logout</button>
+                </div>
+            </div>
         </nav>
         <router-view/>
     </div>
